6b317a6563983043e0b70eb92930da42e2f69338
unknown <yararat@WP81.thrillworksinc.com>
2015-09-08 11:26:21 -0400

    adding the full foundationpress theme

---
+++ wp-content/themes/FoundationPress-master/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "off-canvas-wrap" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+?>
+
+</section>
+<footer class="row">
+	<?php do_action( 'foundationpress_before_footer' ); ?>
+	<?php dynamic_sidebar( 'footer-widgets' ); ?>
+	<?php do_action( 'foundationpress_after_footer' ); ?>
+</footer>
+
+<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) : ?>
+<a class="exit-off-canvas"></a>
+<?php endif; ?>
+
+	<?php do_action( 'foundationpress_layout_end' ); ?>
+
+<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) : ?>
+	</div>
+</div>
+<?php endif; ?>
+
+<?php wp_footer(); ?>
+<?php do_action( 'foundationpress_before_closing_body' ); ?>
+</body>
+</html>
